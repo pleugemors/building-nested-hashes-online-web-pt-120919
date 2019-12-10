32612717caa4d6a350d9c70f10a71180a9f7d905
Paul Leugemors <pleugemors@yahoo.com>
2019-12-10 10:09:27 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -12,7 +12,9 @@ def fourth_challenge
           attitude: "worried"
         },
         {
-          
+          name: "Mercutio",
+          age: "18",
+          attitude: "hot-headed"
         }
       ]
    }, 
@@ -22,6 +24,16 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        {
+          name: "Steven",
+          age: "30",
+          attitude: "confused"
+        },
+        {
+          name: "Nurse",
+          age: "44",
+          attitude: "worried"
+        }
       ]
    }
 }
